9369e8a22a0af0507b311b5a1fd89e28e792bc38
ILAN BITAN <ilan.bitan@live.biu.ac.il>
2021-09-05 15:17:28 +0300

    Resource Management in LWJGL3

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -1,4 +1,5 @@
 package jade;
+
 import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
@@ -9,23 +10,27 @@ import static org.lwjgl.glfw.GLFW.*;
 public class LevelEditorScene extends Scene {
 
     public LevelEditorScene() {
+
     }
+
     @Override
     public void init() {
         this.camera = new Camera(new Vector2f(-250, 0));
+
         int xOffset = 10;
         int yOffset = 10;
+
         float totalWidth = (float)(600 - xOffset * 2);
         float totalHeight = (float)(300 - yOffset * 2);
         float sizeX = totalWidth / 100.0f;
         float sizeY = totalHeight / 100.0f;
-        float padding = 3;
-       // float padding = 0;
+        float padding = 0;
 
         for (int x=0; x < 100; x++) {
             for (int y=0; y < 100; y++) {
                 float xPos = xOffset + (x * sizeX) + (padding * x);
                 float yPos = yOffset + (y * sizeY) + (padding * y);
+
                 GameObject go = new GameObject("Obj" + x + "" + y, new Transform(new Vector2f(xPos, yPos), new Vector2f(sizeX, sizeY)));
                 go.addComponent(new SpriteRenderer(new Vector4f(xPos / totalWidth, yPos / totalHeight, 1, 1)));
                 this.addGameObjectToScene(go);
@@ -41,7 +46,6 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void update(float dt) {
-        System.out.println("FPS: " + (1.0f / dt));
         if (KeyListener.isKeyPressed(GLFW_KEY_RIGHT)) {
             camera.position.x += 100f * dt;
         } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT)) {
@@ -56,6 +60,7 @@ public class LevelEditorScene extends Scene {
         for (GameObject go : this.gameObjects) {
             go.update(dt);
         }
+
         this.renderer.render();
     }
 }
